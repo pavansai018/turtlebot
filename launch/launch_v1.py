4141c3e097b237f9b7571025ff6bfd6a0d80d11f
pavansai018 <18pavansai@gmail.com>
2026-02-12 19:51:10 +0800

    added imu sensor

--- launch/launch_v1.py
+++ launch/launch_v1.py
@@ -60,7 +60,7 @@ def generate_launch_description():
             "/tf@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V",
             '/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model',
             '/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
-            # '/imu/data@sensor_msgs/msg/Imu@gz.msgs.IMU',
+            '/imu/data@sensor_msgs/msg/Imu@gz.msgs.IMU',
             # '/camera@sensor_msgs/msg/Image@gz.msgs.Image',
         ],
     )
