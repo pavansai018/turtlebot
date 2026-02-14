b6d0baab88957d02149a51a97e0f3422a7b133e4
pavansai018 <18pavansai@gmail.com>
2026-02-15 00:48:39 +0800

    added new mazes

--- setup.py
+++ setup.py
@@ -4,6 +4,17 @@ from glob import glob
 
 package_name = 'turtlebot'
 
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            # Construct the full local path
+            local_path = os.path.join(path, filename)
+            # Construct the destination path (share/package_name/path)
+            install_path = os.path.join('share', package_name, path)
+            paths.append((install_path, [local_path]))
+    return paths
+
 setup(
     name=package_name,
     version='0.0.0',
@@ -20,15 +31,17 @@ setup(
         (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.*'))),
         (os.path.join('share', package_name, 'models'), glob(os.path.join('models', '*.*'))),
         (os.path.join('share', package_name, 'maps'), glob(os.path.join('maps', '*.*'))),
-        (os.path.join('share', package_name, 'models/circular_maze'), glob(os.path.join('models/circular_maze', '*.*'))),
-        (os.path.join('share', package_name, 'models/circular_maze/meshes'), glob(os.path.join('models/circular_maze/meshes', '*.*'))),
-        (os.path.join('share', package_name, 'models/square_maze'), glob(os.path.join('models/square_maze', '*.*'))),
-        (os.path.join('share', package_name, 'models/square_maze/meshes'), glob(os.path.join('models/square_maze/meshes', '*.*'))),
-        (os.path.join('share', package_name, 'models/simple_maze'), glob(os.path.join('models/simple_maze', '*.*'))),
-        (os.path.join('share', package_name, 'models/simple_maze/meshes'), glob(os.path.join('models/simple_maze/meshes', '*.*'))),
-        (os.path.join('share', package_name, 'models/turtlebot'), glob(os.path.join('models/turtlebot', '*.*'))),
-        (os.path.join('share', package_name, 'models/turtlebot/meshes'), glob(os.path.join('models/turtlebot/meshes', '*.*'))),
-
+        *package_files('models'),
+        # (os.path.join('share', package_name, 'models/circular_maze'), glob(os.path.join('models/circular_maze', '*.*'))),
+        # (os.path.join('share', package_name, 'models/circular_maze/meshes'), glob(os.path.join('models/circular_maze/meshes', '*.*'))),
+        # (os.path.join('share', package_name, 'models/square_maze'), glob(os.path.join('models/square_maze', '*.*'))),
+        # (os.path.join('share', package_name, 'models/square_maze/meshes'), glob(os.path.join('models/square_maze/meshes', '*.*'))),
+        # (os.path.join('share', package_name, 'models/simple_maze'), glob(os.path.join('models/simple_maze', '*.*'))),
+        # (os.path.join('share', package_name, 'models/simple_maze/meshes'), glob(os.path.join('models/simple_maze/meshes', '*.*'))),
+        # (os.path.join('share', package_name, 'models/turtlebot'), glob(os.path.join('models/turtlebot', '*.*'))),
+        # (os.path.join('share', package_name, 'models/turtlebot/meshes'), glob(os.path.join('models/turtlebot/meshes', '*.*'))),
+        # (os.path.join('share', package_name, 'models/maze_4_metal_6x6'), glob(os.path.join('models/maze_4_metal_6x6', '*.*'))),
+        # (os.path.join('share', package_name, 'models/maze_4_metal_6x6/meshes'), glob(os.path.join('models/maze_4_metal_6x6/meshes', '*.*'))),
 
 
     ],
@@ -46,7 +59,6 @@ setup(
     entry_points={
         'console_scripts': [
             'arrow_teleop = turtlebot.arrow_teleop:main',
-            'odom_to_tf = turtlebot.odom_to_tf:main',
         ],
     },
 )
